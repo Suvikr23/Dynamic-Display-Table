e56a5efe63544c76279016d7ad2a43483760dca2
Suvendu Karmakar <suvi.k023@gmail.com>
2017-11-14 09:14:18 +0530

    Final Commit

--- src/app/d-table/d-table.component.ts
+++ src/app/d-table/d-table.component.ts
@@ -3,7 +3,45 @@ import { Component, OnInit } from '@angular/core';
 // importing Services
 import { JsonService } from '../_shared/json.service';
 
+// calling objects
 import { Company } from '../company';
+
+// Data to be inserted into DB
+const dropDownData : Company[] = [
+  {
+    "id": 2,
+    "name": "Shopsense",
+    "description": "Shopsense is an online shopping destination for electronics, books, music, and movies",
+    "location": "Mumbai",
+    "website": "www.shopsense.co",
+    "market": ["e-commerce", "B2C", "Internet of things", "Retail", "Fashion"],
+    "employees": 41,
+    "stage": "Pre Series A",
+    "founders": ["Ravi Gupta", "Binny Bansal"]
+  },
+  {
+    "id": 3,
+    "name": "Clairvolex",
+    "description": "Clairvolex is an intellectual property solutions for electronics, books, music, and movies",
+    "location": "Gurgaon",
+    "website": "www.clairvolex.com",
+    "market": ["B2B", "Legal"],
+    "employees": 97,
+    "stage": "Series A",
+    "founders": ["Ravi Gupta", "Binny Bansal"]
+  },
+  {
+    "id": 4,
+    "name": "Urban Clap",
+    "description": "Mobile first open marketplace for electronics, books, music, and movies",
+    "location": "Delhi",
+    "website": "www.ubanclap.com",
+    "market": ["Consumer Services", "E-Commerce", "Marketplace", "Local Services"],
+    "employees": 188,
+    "stage": "Series B",
+    "founders": ["Ravi Gupta", "Binny Bansal"]
+  }
+];
 
 @Component({
   selector: 'app-d-table',
@@ -14,7 +52,8 @@ import { Company } from '../company';
 export class DTableComponent implements OnInit {
   jsonData: Company[];
   tableHeadingList: string[] = ['Location', 'Market', 'Website', 'Employees', 'Stage', 'Founders'];
-  comDropdownData : string[] = ['Shopsense', 'Clairvolex', 'AhaTaxis'];
+  comDropdownData : string[] = ['Shopsense', 'Clairvolex', 'Urban Clap'];
+  dropDataCopy = dropDownData;
 
   constructor(private jsonService: JsonService) { 
     this.callJson();
@@ -29,24 +68,14 @@ export class DTableComponent implements OnInit {
     });
   }
 
-  // checkDbUpdate() {
-  //   this.jsonService.updateCompanyData().subscribe ( res => {
-  //     console.log(res);
-  //    this.callJson();
-  //   });
-  // }
-
-  // showCompaniesList(company) {
-  //   if(!company) {
-  //     company = new Company();
-  //     this.isNewData = true;
-  //   }
-  //   this.newComData = company;
-  // }
-
-  addprop1(e) {
-    if(e.target.checked) {
-      
+  addProperty(event, index) {
+    if(event.target.checked) {
+      alert(this.comDropdownData[index]+" is been added");
+      this.jsonService.updateCompanyData(this.dropDataCopy[index]).subscribe ( res => {
+        this.dropDataCopy.splice(index, 1);
+        this.comDropdownData.splice(index, 1);
+        this.callJson();
+        });
     }
   }
 }
